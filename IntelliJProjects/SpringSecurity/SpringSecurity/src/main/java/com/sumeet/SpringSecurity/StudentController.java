036a4bec1ebfa4fd7017bbd9e247643a86f82a8a
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-08-04 18:36:29 +0530

    get csrf-token, get students, post student

---
+++ IntelliJProjects/SpringSecurity/SpringSecurity/src/main/java/com/sumeet/SpringSecurity/StudentController.java
@@ -0,0 +1,37 @@
+package com.sumeet.SpringSecurity;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+    List<Student> studentList= new ArrayList<>(List.of(
+            new Student(1, "sumeet", 10),
+            new Student(2, "tapesh", 9),
+            new Student(3, "abhijeet", 9)
+    ));
+
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+    @GetMapping("/students")
+    public List<Student> getAllStudent(){
+        return studentList;
+    }
+
+    @PostMapping("/create/student")
+    public Student addStudent(@RequestBody Student student){
+        studentList.add(student);
+        return student;
+    }
+}
