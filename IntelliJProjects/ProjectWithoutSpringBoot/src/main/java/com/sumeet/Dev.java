166a8091b640c41443637d464d2bf337266b3134
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-08-06 01:08:26 +0530

    constructor injection of values

--- IntelliJProjects/ProjectWithoutSpringBoot/src/main/java/com/sumeet/Dev.java
+++ IntelliJProjects/ProjectWithoutSpringBoot/src/main/java/com/sumeet/Dev.java
@@ -1,9 +1,29 @@
-package org.SUMEET;
+package com.sumeet;
 
 public class Dev {
+
+    private int age;
+
+
+    public Dev(){
+        System.out.println("dev constructor");
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public Dev(int age){
+        this.age=age;
+        System.out.println("parameterised dev constructor");
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public void build(){
 
         System.out.println("building this project without springboot");
+
     }
 }
