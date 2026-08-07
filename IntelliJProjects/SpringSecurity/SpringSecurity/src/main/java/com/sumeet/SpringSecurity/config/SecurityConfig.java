e371eb1036af9b6bb460ca47b9224cb2e1a15ebb
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-08-08 00:11:09 +0530

    builder pattern changes

--- IntelliJProjects/SpringSecurity/SpringSecurity/src/main/java/com/sumeet/SpringSecurity/config/SecurityConfig.java
+++ IntelliJProjects/SpringSecurity/SpringSecurity/src/main/java/com/sumeet/SpringSecurity/config/SecurityConfig.java
@@ -16,11 +16,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
 
-        httpSecurity.csrf(customizer->customizer.disable());
-        httpSecurity.authorizeHttpRequests(request-> request.anyRequest().authenticated());
+//        httpSecurity.csrf(customizer->customizer.disable());      ->disables csrf token, hence no csrf token needed for state change calls
+//        httpSecurity.authorizeHttpRequests(request-> request.anyRequest().authenticated());  ->enables authorization required in custom security filter chain
+//        httpSecurity.formLogin(Customizer.withDefaults());                 -> enables form login in browser, disable this to work with session creation policy so that new login is not required each time
+//        httpSecurity.httpBasic(Customizer.withDefaults());                -> enables working in api calls using postman etc
+//        httpSecurity.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));   ->enables stateless
+
+        //builder pattern
+        httpSecurity
+                .csrf(customizer->customizer.disable())
+                .authorizeHttpRequests(request-> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(
+                        session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 //        httpSecurity.formLogin(Customizer.withDefaults());
-        httpSecurity.httpBasic(Customizer.withDefaults());
-        httpSecurity.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return httpSecurity.build();
     }
 }
