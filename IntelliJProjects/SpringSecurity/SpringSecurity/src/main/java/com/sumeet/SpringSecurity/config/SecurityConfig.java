7e12c003ec5bd4fa554749314c49b4bf70507936
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-08-08 22:33:46 +0530

    added custom userdetail service

--- IntelliJProjects/SpringSecurity/SpringSecurity/src/main/java/com/sumeet/SpringSecurity/config/SecurityConfig.java
+++ IntelliJProjects/SpringSecurity/SpringSecurity/src/main/java/com/sumeet/SpringSecurity/config/SecurityConfig.java
@@ -3,10 +3,15 @@ package com.sumeet.SpringSecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,9 +23,15 @@ public class SecurityConfig {
 
 //        httpSecurity.csrf(customizer->customizer.disable());      ->disables csrf token, hence no csrf token needed for state change calls
 //        httpSecurity.authorizeHttpRequests(request-> request.anyRequest().authenticated());  ->enables authorization required in custom security filter chain
-//        httpSecurity.formLogin(Customizer.withDefaults());                 -> enables form login in browser, disable this to work with session creation policy so that new login is not required each time
+//        httpSecurity.formLogin(Customizer.withDefaults());
 //        httpSecurity.httpBasic(Customizer.withDefaults());                -> enables working in api calls using postman etc
 //        httpSecurity.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));   ->enables stateless
+
+        //formlogin used to authenticate once and keep user logged in using http session,
+        // but stateless session creation policy enables keeping no session,
+        // hence after succesfull form login there is nowhere to store
+        // the session id hence formlogin and stateless session cration policy
+        // should not be used together
 
         //builder pattern
         httpSecurity
@@ -33,4 +44,35 @@ public class SecurityConfig {
 
         return httpSecurity.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1= User
+                .withDefaultPasswordEncoder()
+                .username("thesumeetsingh")
+                .password("sumeet@123")
+                .roles("ADMIN")
+                .build();
+        UserDetails user2= User
+                .withDefaultPasswordEncoder()
+                .username("rahul")
+                .password("rahul@123")
+                .roles("TEACHER")
+                .build();
+        UserDetails user3= User
+                .withDefaultPasswordEncoder()
+                .username("peterparker")
+                .password("peter@123")
+                .roles("STUDENT")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(user1, user2, user3);
+    }
+
+//    @Bean
+//    public AuthenticationProvider authenticationProvider(){
+//
+//    }
 }
