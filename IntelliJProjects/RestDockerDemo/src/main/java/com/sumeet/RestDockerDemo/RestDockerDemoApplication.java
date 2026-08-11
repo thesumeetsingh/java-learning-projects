6ee9c7c8323397fcb0959dbe167bd3140ac99ea5
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-08-11 18:50:23 +0530

    docker demo

---
+++ IntelliJProjects/RestDockerDemo/src/main/java/com/sumeet/RestDockerDemo/RestDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sumeet.RestDockerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDockerDemoApplication.class, args);
+	}
+
+}
